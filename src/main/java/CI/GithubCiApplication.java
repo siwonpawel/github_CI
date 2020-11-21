eb00a81d36ef17b76172c3b2a64c4af180183853
siwonpawel <siwonpawel@gmail.com>
2020-11-21 12:43:29 +0100

    Initial commit

---
+++ src/main/java/CI/GithubCiApplication.java
@@ -0,0 +1,13 @@
+package CI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CI/GithubCiApplicationTests.java
@@ -0,0 +1,13 @@
+package CI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubCiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
